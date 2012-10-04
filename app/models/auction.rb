adae8bf43abaa73ffcc2f82323b4b28536d82055
Jason Atwood <jwatwood@gmail.com>
2012-10-04 16:35:49 -0400

    adds validation tests of auction model core attributes

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,5 +1,7 @@
 class Auction < ActiveRecord::Base
-   attr_accessible :id, :price, :Product, :baseinfo, :seller_name, :start_time
+
+	attr_accessible :id, :price, :Product, :baseinfo, :seller_name, :start_time
   
+	validates :id, :price, :Product, :start_time, :presence => true
   
 end
--- spec/models/auction_spec.rb
+++ spec/models/auction_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
-describe Auction do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+
+describe Auction do  # tests for the model
+	# model attributes :id, :price, :Product, :baseinfo, :seller_name, :start_time
+	testAuction = Auction.new()
+
+	it 'is not valid without an id' do
+		testAuction.should_not be_valid
+	end	
+	
+	it 'is not valid without a price' do
+		# not the best way of doing this
+		testAuction.id = 0
+		testAuction.should_not be_valid
+	end
+	
+	it 'is not valid without a Product' do
+		testAuction.price = 12.00
+		testAuction.should_not be_valid
+	end
+	
+	it 'is not valide without a start time' do
+		testAuction.Product = 'dummy product name'
+		testAuction.should_not be_valid
+	end
 end

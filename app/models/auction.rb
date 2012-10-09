91a466f967a3703b92a637a364760deb24688048
Yao <haoy@andrew.cmu.edu>
2012-10-09 16:51:53 -0400

    hao/jason pt: 37458879, 37459629 removes auction ID validation test, removes attrib_accesible from aution model

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,11 +1,7 @@
 class Auction < ActiveRecord::Base
 
-
 	#attr_accessible  :price, :product, :baseinfo, :seller_name, :start_time
   
 	validates  :price, :product, :start_time, :presence => true
-  
-
-
 
 end
--- spec/models/auction_spec.rb
+++ spec/models/auction_spec.rb
@@ -6,13 +6,8 @@ describe Auction do  # tests for the model
 	# model attributes :id, :price, :Product, :baseinfo, :seller_name, :start_time
 	testAuction = Auction.new()
 
-	it 'is not valid without an id' do
-		testAuction.should_not be_valid
-	end	
-	
 	it 'is not valid without a price' do
 		# not the best way of doing this
-		testAuction.id = 0
 		testAuction.should_not be_valid
 	end
 	

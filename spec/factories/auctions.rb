03a9dbfc7edb0fbf2b32ed3b4cde3da8dba37477
Jason Atwood <jwatwood@gmail.com>
2012-10-12 19:40:31 -0400

    adds factory for auction model testing

--- spec/factories/auctions.rb
+++ spec/factories/auctions.rb
@@ -1,6 +1,18 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :auction do
+
+  factory :emptyAuction, class:Auction do
+	#set no attributes
+  end
+  
+  
+  factory :auction, class:Auction do
+	seller_name 'bob'
+	price		10.00
+	product		'book shelf'
+	baseinfo	'tall, wooden 3 shelves'
+	start_time 	'12:00:00'
+	 
   end
 end

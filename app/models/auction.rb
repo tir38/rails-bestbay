dbf8edd3c39676119d4dd942b9844c44f8d96b4e
Jason Atwood <jwatwood@gmail.com>
2012-10-23 13:36:20 -0400

    adds annotate gem and performs annotation

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: auctions
+#
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  price              :decimal(, )
+#  id                 :integer
+#  product            :string(255)
+#  seller_name        :string(255)
+#  baseinfo           :string(255)
+#  start_time         :string(255)
+#  highestBidderEmail :string(255)
+#
+
 class Auction < ActiveRecord::Base
 
 	attr_accessible  :price, :product, :baseinfo, :seller_name, :start_time
--- spec/models/auction_spec.rb
+++ spec/models/auction_spec.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: auctions
+#
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  price              :decimal(, )
+#  id                 :integer
+#  product            :string(255)
+#  seller_name        :string(255)
+#  baseinfo           :string(255)
+#  start_time         :string(255)
+#  highestBidderEmail :string(255)
+#
+
 require 'spec_helper'
 describe Auction do  # tests for the model
 	# model attributes :id, :price, :Product, :baseinfo, :seller_name, :start_time

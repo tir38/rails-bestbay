ec3ac1aa4dbb81537b6572372d590f59a7ee4495
Jason Atwood <jwatwood@gmail.com>
2012-10-24 17:50:16 -0400

    deletes temp migrations

--- db/migrate/20121017150642_add_bid_to_auctions.rb
+++
@@ -1,5 +0,0 @@
-class AddBidToAuctions < ActiveRecord::Migration
-  def change
-    add_column :auctions, :bid, :boolean
-  end
-end
--- db/migrate/20121017150642_add_bid_to_auctios.rb
+++
@@ -1,5 +0,0 @@
-class AddBidToAuctios < ActiveRecord::Migration
-  def change
-    add_column :auctios, :bid, :boolean
-  end
-end
